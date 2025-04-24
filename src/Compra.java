96953e27a9b2284040c4b15b92d50df009e5913a
Santiago Ferreira <santiandresfer@hotmail.com>
2025-04-24 16:36:41 -0300

    App para lanzar compras en una tarjeta de credito

---
+++ src/Compra.java
@@ -0,0 +1,22 @@
+public class Compra {
+    private String descripcion;
+    private double valor;
+
+    public Compra(double valor, String descripcion) {
+        this.valor = valor;
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Compra: valor= " + valor + ", descripcion= " + descripcion;
+    }
+}
